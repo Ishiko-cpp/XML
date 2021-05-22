ad81abe504b927b5caceb3ed51736a43de9a0d81
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-05-23 00:57:53 +0100

    Updates

---
+++ Include/Ishiko/XML.h
@@ -0,0 +1,18 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/XML/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_XML_H_
+#define _ISHIKO_XML_H_
+
+#include "XML/XMLFile.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "IshikoXML-d.lib")
+#else
+#pragma comment(lib, "IshikoXML.lib")
+#endif
+
+#endif
---
+++ Include/Ishiko/XML/XMLFile.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/XML/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_XML_XMLFILE_H_
+#define _ISHIKO_XML_XMLFILE_H_
+
+#endif
