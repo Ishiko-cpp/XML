334912eaef930fb397c8e7360d313a75e004cdaa
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-05-23 17:07:40 +0100

    Skeleton libraries and tests (#1)

---
+++ Include/Ishiko/XML/Document.h
@@ -0,0 +1,22 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/XML/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_XML_DOCUMENT_H_
+#define _ISHIKO_XML_DOCUMENT_H_
+
+namespace Ishiko
+{
+namespace XML
+{
+
+class Document
+{
+};
+
+}
+}
+
+#endif
---
+++ Tests/Source/main.cpp
@@ -0,0 +1,20 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/XML/blob/master/LICENSE.txt
+*/
+
+#include "DocumentTests.h"
+#include <Ishiko/TestFramework/TestFrameworkCore.h>
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("IshikoXML");
+
+    TestSequence& theTests = theTestHarness.tests();
+    theTests.append<DocumentTests>();
+    
+    return theTestHarness.run();
+}
