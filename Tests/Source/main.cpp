ef9b7e8b608cd4a3832ff07d6590c5f190e70ad5
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-05-23 14:43:53 +0100

    Add tests

---
+++ Tests/Source/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/Ishiko-cpp/XML/blob/master/LICENSE.txt
+*/
+
+#include <Ishiko/TestFramework/TestFrameworkCore.h>
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("IshikoXML");
+    
+    return theTestHarness.run();
+}
