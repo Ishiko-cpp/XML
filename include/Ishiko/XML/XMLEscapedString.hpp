12fbe9ad8ce923ceeb9aefd549359bda3717ce52
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-07-05 20:00:23 +0100

    Add files

---
+++ include/Ishiko/XML/XMLEscapedString.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/xml/blob/master/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_XML_XMLESCAPEDSTRING_HPP_
+#define _ISHIKO_CPP_XML_XMLESCAPEDSTRING_HPP_
+
+#endif
