dc2649dd7296abf79fa702a6911bc4d148d5f58b
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-07-15 19:34:05 +0100

    Add files

---
+++ include/Ishiko/XML/XMLPushParser.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/xml/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_XML_XMLPUSHPARSER_HPP_
+#define _ISHIKO_CPP_XML_XMLPUSHPARSER_HPP_
+
+#endif
--- src/XMLEscapedString.cpp
+++ src/XMLEscapedString.cpp
@@ -1,7 +1,7 @@
 /*
     Copyright (c) 2022 Xavier Leclercq
     Released under the MIT License
-    See https://github.com/ishiko-cpp/xml/blob/master/LICENSE.txt
+    See https://github.com/ishiko-cpp/xml/blob/main/LICENSE.txt
 */
 
 #include "XMLEscapedString.hpp"
